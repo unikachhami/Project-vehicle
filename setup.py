5cfd73f5aaa54d19baa963f0665fc4408e9ca044
Unik <unikbahadur1852@gmail.com>
2026-02-13 17:00:05 +0545

    project structure established

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="unikachhami",
+    author_email="unikbahadur1852@gmail.com",
+    packages =find_packages(),
+)
